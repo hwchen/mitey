fcbff5e1a9ea3eed3d04304782ea7ee7d88f7538
Walther Chen <walther.chen@gmail.com>
2020-04-20 23:36:27 -0400

    Readme thoughts, example with 2 ports
    Expand thoughts on philosophy of mitey, as well as one advantage: full
    power to the programmer for orchestrating runtimes, with mitey stepping
    in just the right amount.
    
    Example (an ugly one so far) of this in async-std example.

--- README.md
+++ README.md
@@ -1,20 +1,35 @@
 # mitey
 
-Small and mighty.
+A web framework for Rust.
 
-Yet another web framework (in Rust).
+Core values:
+- Minimal abstractions.
+- Minimal layers.
+- Compatibility with the widest range of async runtimes and io primitives.
+- You call the library, instead of the framework calling you.
 
-Created on the smallest, simplest, and most transparent async stack possible.
+My hope is that ease of use comes out of being able to understand the system, instead of by hiding complexity behind an ergonomic interface.
 
-# POC
-So far I've made the library "independent" of the tcp stream. I can see how it should be able to hook into any tcp stream that implements AsyncWrite and AsyncRead (or even better, async-ified std::TcpStream via smol). But now I need to figure out how to plug an executor into the library? Or am I missing another step.
+In addition, in order to be able to interact with the widest range of async runtimes and io primitives, there will be some necessary boilerplate, as well possibly implementing some executor details.
+
+## Web Framework Random Thoughts
+
+The underlying goal is to provide the basics of routing and state management without interfering with the user's ability to when and where the web server interacts with async runtimes. For example, most current web frameworks have a `run` or `serve` method which is the final blocking call in `fn main`, and it basically spins up a loop for spawning new tasks (http connections). The web framework controls that loop, so it's not possible for another server to have its own loop to spawn tasks, unless it's on another thread.
+
+But there's no particular reason that a web framework needs to monopolize this task-spawning loop. For example, if you want different parts of the app to bind to multiple ports with different protocols. Mitey gets out of the way, and lets the programmer orchestrate all the io streams as necessary, and only steps in to route and handle the http requests on whatever stream is needed.
+
+The programmer is also able to orchestrate threads and async runtimes in whatever configuration is necessary for highest performance, Mitey can be passed around, refernced, cloned, or whatever is necessary. Mitey does not impose a runtime architecture.
+
+In essence, for complex apps, Mitey will help you feel like it's just a part of the system, as opposed a force you have to either go along with or work around.
+
+## Previous random thoughts
 
 Is it possible to provide a web framework so minimal that execution details can be controlled by the user, only calling into the web framework for convenience? What is the minimal feature set? And who would use this kind of library?
 
 For basic REST apis and CRUD frameworks, people probably want a simple all-in-one, so something like actix, rocket or warp would be the easiest. They provide routing, middleware, and more convenient error handling. It's easy for users to access Req, Resp, state, and router vars all in one function, and just write that fn.
 
-The main pain point for non-power users is trying to incorporate a library that depends on a different runtime.
+The main pain point for non-power users is trying to incorporate a library that depends on a different runtime?
 
-For power users, they would already go down to hyper, and maybe write their own routing library, and just do error handling on their own, perhaps with their own wrapper. 
+For power users, they would already go down to hyper, and maybe write their own routing library, and just do error handling on their own, perhaps with their own wrapper.
 
 Well, whatever. Still going to give a shot at a cross-executor web framework.
--- examples/hello-async-std.rs
+++ examples/hello-async-std.rs
@@ -6,36 +6,64 @@ use mitey::{Router, State};
 
 #[async_std::main]
 async fn main() -> http_types::Result<()> {
-    // State (database)
-    let state = State::init("mitey-state".to_owned());
+    // server 1
+    let state_1 = State::init("mitey-state_1".to_owned());
 
-    // Routing
-    // not a real router, so can't add another route
-    let mut router = Router::new();
-    router.add_route("/one", handle_one);
-    let router = router.init();
+    let mut router_1 = Router::new();
+    router_1.add_route("/one", handle_one);
+    let router_1 = router_1.init();
 
-    // Now tcp IO
-    let listener = TcpListener::bind(("127.0.0.1", 8080)).await?;
-    let addr = listener.local_addr()?;
-    let addr = format!("http://{}", addr); // TODO fix hack
-    println!("mitey serving at {}", addr);
+    let listener_1 = TcpListener::bind(("127.0.0.1", 8080)).await?;
+    let addr_1 = format!("http://{}", listener_1.local_addr()?); // TODO fix hack
+    println!("mitey serving at {}", addr_1);
 
-    let mut incoming = listener.incoming();
-    while let Some(stream) = incoming.next().await {
-        let stream = stream?;
+    let incoming_1 = listener_1.incoming();
 
-        let addr = addr.clone();
-        let state = state.clone();
-        let router = router.clone();
+    let serve_1 = incoming_1.for_each(|stream| {
+        let stream = stream.unwrap();
+
+        let addr_1 = addr_1.clone();
+        let state_1 = state_1.clone();
+        let router_1 = router_1.clone();
 
         // Now spawn into mitey handler, with all components
         task::spawn( async move {
-            if let Err(err) = mitey::accept(addr, stream, state, router).await {
+            if let Err(err) = mitey::accept(addr_1, stream, state_1, router_1).await {
                 eprintln!("{}", err);
             }
         });
-    }
+    });
+
+    // server 2
+    let state_2 = State::init("mitey-state_2".to_owned());
+
+    let mut router_2 = Router::new();
+    router_2.add_route("/two", handle_two);
+    let router_2 = router_2.init();
+
+    let listener_2 = TcpListener::bind(("127.0.0.1", 8081)).await?;
+    let addr_2 = format!("http://{}", listener_2.local_addr()?); // TODO fix hack
+    println!("mitey serving at {}", addr_2);
+
+    let incoming_2 = listener_2.incoming();
+
+    let serve_2 = incoming_2.for_each(|stream| {
+        let stream = stream.unwrap();
+
+        let addr_2 = addr_2.clone();
+        let state_2 = state_2.clone();
+        let router_2 = router_2.clone();
+
+        // Now spawn into mitey handler, with all components
+        task::spawn( async move {
+            if let Err(err) = mitey::accept(addr_2, stream, state_2, router_2).await {
+                eprintln!("{}", err);
+            }
+        });
+    });
+
+    // Combine streams (should use just one async runtime)
+    futures_util::future::join(serve_1, serve_2).await;
 
     Ok(())
 }
@@ -43,13 +71,27 @@ async fn main() -> http_types::Result<()> {
 async fn handle_one(_req: Request) -> http_types::Result<Response> {
     let mut res = Response::new(StatusCode::Ok);
     res.insert_header("Content-Type", "text/plain")?;
-    res.set_body("mitey: small and mighty");
+    res.set_body("mitey: small and mighty 1");
 
     //task::spawn_blocking(|| {
     task::block_on(async {
-        println!("long-running task started");
+        println!("long-running task_1 started");
         std::thread::sleep(std::time::Duration::from_millis(10_000));
-        println!("long-running task ended");
+        println!("long-running task_1 ended");
+    });
+    Ok(res)
+}
+
+async fn handle_two(_req: Request) -> http_types::Result<Response> {
+    let mut res = Response::new(StatusCode::Ok);
+    res.insert_header("Content-Type", "text/plain")?;
+    res.set_body("mitey: small and mighty 2");
+
+    //task::spawn_blocking(|| {
+    task::block_on(async {
+        println!("long-running task_2 started");
+        std::thread::sleep(std::time::Duration::from_millis(10_000));
+        println!("long-running task_2 ended");
     });
     Ok(res)
 }
